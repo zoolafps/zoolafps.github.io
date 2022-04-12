89f2d39b0e7eb234864966d68245cc459e0b19e6
Andrés Felipe Patiño Sierra <91166247+zoolafps@users.noreply.github.com>
2022-04-12 09:05:32 -0500

    12/04/22
    mejora de rendimiento, visualización y agregue la versión en ingles  :)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
-let container = document.querySelector('.conteiner');
-let layer = document.querySelectorAll('.layer');
-
-container.onmousemove = function(e){
-    let X =e.pageX;
-    let Y =e.pageY;
-
-    layer[0].style.transform = 'translate('+ X/100 +'px, '+ Y/100
+function write(){
+    document.querySelector(".btn-lang").classList.toggle("show");
+    document.querySelector(".box").classList.toggle("show");
+    document.querySelector(".box-1").classList.toggle("show");
 }
+
+//function dewrite(){
+//    document.querySelector(".btn-lang-close").classList.toggle("show");
+//    document.querySelector(".box").innerHTML='';
+//}
+
+document.querySelector(".btn-lang").addEventListener("click", () => {
+    write();
+  });
+
+//document.querySelector(".btn-lang-close").addEventListener("click", () => {
+//    dewrite();
+//  });
