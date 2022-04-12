ee7e04637018c26d60bf2bf9b3dc1bf229767384
Andrés Felipe Patiño Sierra <91166247+zoolafps@users.noreply.github.com>
2022-04-12 17:04:47 -0500

    12/04/22 5:03pm
    mejora de responsividad y preparación para nuevas características

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
+//function change lenguage
 function write(){
     document.querySelector(".btn-lang").classList.toggle("show");
     document.querySelector(".box").classList.toggle("show");
     document.querySelector(".box-1").classList.toggle("show");
 }
-
-//function dewrite(){
-//    document.querySelector(".btn-lang-close").classList.toggle("show");
-//    document.querySelector(".box").innerHTML='';
-//}
-
 document.querySelector(".btn-lang").addEventListener("click", () => {
     write();
   });
 
-//document.querySelector(".btn-lang-close").addEventListener("click", () => {
-//    dewrite();
-//  });
+//funtion hamburguer menu
+function barsbtn(){
+  document.querySelector(".open").classList.toggle("show");
+  document.querySelector(".close").classList.toggle("show");
+  }
+document.querySelector(".open").addEventListener("click", () => {
+  barsbtn()
+});
+document.querySelector(".close").addEventListener("click", () => {
+  barsbtn()
+});
